9ba1bcbcd40f72a9244c53c69b647895a4d8e442
Hector <52350216+Higna@users.noreply.github.com>
2024-10-16 10:58:21 +0100

    update

--- scrapper.py
+++ scrapper.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+import pandas as pd
+import os
 import time
 
 # Path to Chrome driver executable
